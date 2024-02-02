9257e7ee9b7cc394703590a16dc0ffc84ae2d598
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2024-02-02 15:45:31 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Footer from "./components/Footer";
+import Info from "./components/Info";
+import About from "./components/About";
+
+export default function App() {
+  return (
+    <div className="App">
+      <About />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
